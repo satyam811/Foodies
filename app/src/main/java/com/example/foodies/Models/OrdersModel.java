e8db00127bcc4c51bef2d07fe01d1eabbec9d2c0
Satyam Gupta <satyamx811@gmail.com>
2022-07-09 15:15:27 +0530

    Orders page Designed Complete

---
+++ app/src/main/java/com/example/foodies/Models/OrdersModel.java
@@ -0,0 +1,46 @@
+package com.example.foodies.Models;
+
+public class OrdersModel {
+
+    int orderImaage;
+    String soldItemName, price, orderNumber;
+
+    public OrdersModel(int orderImaage, String soldItemName, String price, String orderNumber) {
+        this.orderImaage = orderImaage;
+        this.soldItemName = soldItemName;
+        this.price = price;
+        this.orderNumber = orderNumber;
+    }
+
+    public int getOrderImaage() {
+        return orderImaage;
+    }
+
+    public void setOrderImaage(int orderImaage) {
+        this.orderImaage = orderImaage;
+    }
+
+    public String getSoldItemName() {
+        return soldItemName;
+    }
+
+    public void setSoldItemName(String soldItemName) {
+        this.soldItemName = soldItemName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+}
--- app/src/main/java/com/example/foodies/OrderActivity.java
+++ app/src/main/java/com/example/foodies/OrderActivity.java
@@ -1,14 +1,46 @@
 package com.example.foodies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+
+import com.example.foodies.Adapters.OrdersAdapter;
+import com.example.foodies.Models.OrdersModel;
+import com.example.foodies.databinding.ActivityOrderBinding;
+
+import java.util.ArrayList;
 
 public class OrderActivity extends AppCompatActivity {
+
+    ActivityOrderBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order);
+        binding = ActivityOrderBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ArrayList<OrdersModel> list = new ArrayList<>();
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+        list.add(new OrdersModel(R.drawable.burger, "Chees Burgur", "59", "1234884"));
+
+
+        OrdersAdapter adapter = new OrdersAdapter(list, this);
+        binding.orderRecyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.orderRecyclerView.setLayoutManager(layoutManager);
+
+
     }
 }
